588d9a361a1dbd97657ac8b65aa7a7eb3ba6976b
SamriddhiMundra <samriddhimundra01@gmail.com>
2025-06-14 22:59:12 +0530

    new commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,6 +11,17 @@ const signToken = id => {
         expiresIn : process.env.JWT_EXPIRES_IN
     })
 }
+const createSendToken = (user, statusCode, res) => {
+    const token = signToken(user._id);
+
+    res.status(statusCode).json({
+        status: 'success',
+        token,
+        data: {
+            user
+        }
+    })
+}
 
 exports.signup = catchAsync( async (req, res, next) => {
     const newUser = await User.create({
@@ -20,16 +31,8 @@ exports.signup = catchAsync( async (req, res, next) => {
         passwordConfirm: req.body.passwordConfirm  //user cannot become admin!
         //role: req.body.role
     });
-
-    const token = signToken(newUser._id);
-
-    res.status(201).json({
-        status: 'success',
-        token,
-        data: {
-            user: newUser
-        }
-    })
+    createSendToken(newUser, 201, res);
+    
 })
 
 exports.login = catchAsync( async (req, res, next) => {
@@ -49,11 +52,8 @@ exports.login = catchAsync( async (req, res, next) => {
     }
     //console.log(user);
     //3. If everything is fine, send token to client
-    const token = signToken(user._id);
-    res.status(200).json({
-        status: 'success',
-        token
-    })
+    createSendToken(user, 200, res)
+    
 });
 
 exports.protect = catchAsync(async(req, res, next) => {
@@ -140,13 +140,44 @@ catch(err){
 exports.resetPassword = catchAsync( async(req, res, next) => {
     //1. get user based on the token
     const hashedToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
-    const user = await User.findOne({passwordResetToken: hashedToken})
+    const user = await User.findOne({passwordResetToken: hashedToken, passwordResetExpires: {$gt: Date.now()}});
+
 
     //2.if token is not expired and there is user, then set new password
+    if(!user){
+        return next(new AppError('Token is invalid or expired', 400));
+    }
+    user.password = req.body.password;
+    user.passwordConfirm = req.body.passwordConfirm;
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save();
 
     //3. update changedPasswordAt property for the user
 
     //4. log the user in, send jwt
+    createSendToken(user, 200, res)
+    
   });
+
+  exports.updatePassword = catchAsync( async(req, res, next) => {
+    //1. Get the user from collection
+    const user = await User.findById(req.user.id).select('+password');
+
+    //2. Check if posted password is correct
+    if(!(await user.correctPassword(req.body.passwordCurrent, user.password))){
+        return next(new AppError('Your current password is incorrect', 404));
+    }
+
+    //3. If so, update password
+    user.password = req.body.password;
+    user.passwordConfirm = req.body.passwordConfirm;
+    await user.save();
+    //User.findByIdAndUpdate will not work as intended
+
+    //4. log user in, send jwt
+    createSendToken(user, 200, res)
+
+  })
   
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+const AppError = require('../utils/appError');
 const User = require(`./../models/userModel`);
 const catchAsync = require('./../utils/catchAsync');
 
@@ -10,6 +11,19 @@ exports.getAllUsers = catchAsync( async (req, res, next) => {
         data: {users}
     })
 })
+
+exports.updateMe = (req, res, next) => {
+    //1. create error if user POSTs password data
+    if(req.body.password || req.body.passwordConfirm){
+        return next(new AppError('This route is not for password updates. Please use updateMyPassword', 400));
+
+    }
+
+    //2 update user document
+    res.status(200).json({
+        status: 'success'
+    })
+}
 exports.getUser = (req, res) => {
     res.status(500).json({
         status: 'error',
