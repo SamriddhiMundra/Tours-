d8b7e83fa0cd4de6176f4c07b2e43decd029a940
SamriddhiMundra <samriddhimundra01@gmail.com>
2025-06-14 13:17:01 +0530

    password reset

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,6 +16,7 @@ exports.signup = catchAsync( async (req, res, next) => {
         email: req.body.email,
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm  //user cannot become admin!
+        //role: req.body.role
     });
 
     const token = signToken(newUser._id);
@@ -85,3 +86,33 @@ exports.protect = catchAsync(async(req, res, next) => {
    req.user = currentUser;
     next()
 })
+
+exports.restrictTo = (...roles) => {
+    return (req, res, next) => {
+        //roles ['admin', 'lead-guide'] role='user'
+        if(!roles.includes(req.user.role)){
+            return next(new AppError('You do not have permission to perform the action', 403))
+        }
+        next();
+    }
+}
+
+exports.forgotPassword = catchAsync( async(req, res, next) => {
+//1. Get user based on POSTED email
+const user = await User.findOne({ email: req.body.email});
+if(!user){
+    return next(new AppError('There is no user with this email address', 404));
+}
+
+//2. Generate random reset token.
+//create instance method of user in usermodel
+const resetToken = user.createPasswordResetToken();
+await user.save({validateBeforeSave: false});
+
+//3. Send it to user's email
+})
+
+exports.resetPassword = (req, res, next) => {
+
+}
+
--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -17,6 +18,11 @@ const userSchema = new mongoose.Schema({
       photo: {
         type: String
       },
+      role: {
+        type: String,
+        enum: ['user', 'guide', 'lead-guide', 'admin'],
+        default: 'user'
+      },
       password: {
         type: String,
         required: [true, 'Password is required'],
@@ -35,7 +41,9 @@ const userSchema = new mongoose.Schema({
           message: 'Passwords are not the same'
         }
       },
-      passwordChangedAt: Date
+      passwordChangedAt: Date,
+      passwordResetToken: String,
+      passwordResetExpires: Date
 })
 
 
@@ -67,6 +75,14 @@ userSchema.methods.changePassword = async function(JWTTimestamp){
   }
   //false means not changed
   return false;
+}
+
+userSchema.methods.createPasswordResetToken = function(){
+const resetToken = crypto.randomBytes(32).toString('hex');
+this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+console.log({resetToken}, this.passwordResetToken )
+this.passwordResetExpires = Date.now() + 10*60*1000;
+return resetToken;
 }
 
 const User = mongoose.model('User', userSchema);
