4fae9c9a1b3255563bff8e3dac625aa52e3f842f
SamriddhiMundra <samriddhimundra01@gmail.com>
2025-06-18 15:40:48 +0530

    new commit

--- app.js
+++ app.js
@@ -1,23 +1,61 @@
 /* eslint-disable import/no-dynamic-require */
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const app = express();
+
+//1 GLOBAL MIDDLEWARES
+
+//set security HTTP headers
+app.use(helmet());
+
+//Development logging
 if (process.env.NODE_ENV === 'development') {
   //this is available in every file in project
   app.use(morgan('dev'));
 }
+
+//Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60*60*1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require(`${__dirname}/routes/tourRoutes`);
 const userRouter = require(`${__dirname}/routes/userRoutes`);
-app.use(express.json());
+
+//Body Parser, reading data from body into req.body
+app.use(express.json({limit: '10kb'}));
+
+//data sanitization against NoSQL query injections
+app.use(mongoSanitize());
+
+//data sanitization against XSS
+app.use(xss());
+
+//prevent parameter pollution
+app.use(hpp({
+  whitelist: ['duration', 'ratingAverage', 'ratingsQuantity', 'maxGroupSize', 'difficulty', 'price']
+}));
+
+//serving static files
 app.use(express.static(`${__dirname}/public`));
 
 // app.use((req, res, next) => {
 //   console.log('Hello from middleware!');
 //   next();
 // });
+
+//test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   console.log(req.headers);
