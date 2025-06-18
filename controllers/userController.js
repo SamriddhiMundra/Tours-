b50ee371d535223413f2a728872d4e7eb3541f9d
SamriddhiMundra <samriddhimundra01@gmail.com>
2025-06-18 13:23:14 +0530

    new commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,16 @@ const AppError = require('./../utils/appError');
 const User = require(`./../models/userModel`);
 const catchAsync = require('./../utils/catchAsync');
 
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {};
+    Object.keys(obj).forEach(el => {
+        if(allowedFields.includes(el)){
+            newObj[el] = obj[el];
+        }
+    })
+    return newObj;
+}
+
 exports.getAllUsers = catchAsync( async (req, res, next) => {
 
     const users = await User.find();
@@ -16,13 +26,31 @@ exports.getAllUsers = catchAsync( async (req, res, next) => {
 exports.updateMe = catchAsync( async(req, res, next) => {
     //1. create error if user POSTs password data
     if(req.body.password || req.body.passwordConfirm){
-        return next(new AppError('This route is not for password updates. Please use updateMyPassword', 400));
-
+        return next(new AppError('This route is not for password updates. Please use /updateMyPassword', 400));
     }
 
-    //2 update user document
+    //3 Filtered out unwanted field names that are not allowed to be updated
+    const filteredBody = filterObj(req.body, 'name', 'email');
+
+    //3 update user document
+    //findByIdAndUpdate only works for loggedin users
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {new: true,
+        runValidators: true
+    });
     res.status(200).json({
-        status: 'success'
+        status: 'success',
+        data: {
+            user: updatedUser
+        }
+    })
+})
+
+exports.deleteMe = catchAsync(async(req, res, next) => {
+    await User.findByIdAndUpdate(req.user.id, {active: false});
+
+    res.status(204).json({
+        status: 'success',
+        data: null
     })
 })
 exports.getUser = (req, res) => {
--- models/userModel.js
+++ models/userModel.js
@@ -43,7 +43,12 @@ const userSchema = new mongoose.Schema({
       },
       passwordChangedAt: Date,
       passwordResetToken: String,
-      passwordResetExpires: Date
+      passwordResetExpires: Date,
+      active: {
+        type: Boolean,
+        default: true,
+        select: false
+      }
 })
 
 
@@ -66,6 +71,12 @@ userSchema.pre('save', function(next){
   }
   this.passwordChangedAt = Date.now() - 1000;
   next();
+})
+userSchema.pre(/^find/, function(next){
+//it is a query middleware, so "this" points to current query
+this.find({active: {$ne: false}});
+next()
+
 })
 
 //INSTANCE METHOD
